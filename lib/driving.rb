1b197e1999060adfed1780195a70513b94d5c2fa
Skyler Layne <skylerclayne@gmail.com>
2016-05-25 19:13:59 -0400

    start prep and drive

---
+++ lib/driving.rb
@@ -0,0 +1,17 @@
+module Driving
+  class Drive
+    attr_accessor :info
+
+    def initialize
+      @info = "During the driving test, make sure you are always staying aware of what is around you. You should be checking your mirrors, over both shoulders, and
+scanning for any cars, road signs, or pedestrians.
+
+You should also pay attention to your speed. It is important that you **do not
+exceed the speed limit** stay within `-5` of the speed. You should also know
+that unless otherwise posted the speed is *50km/hr*.
+
+If there are 2 lanes, drive in the right. If there are 3 or more, drive in the
+lane directly to the left of the right most lane."
+    end
+  end
+end
