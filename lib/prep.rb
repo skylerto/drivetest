b902f74e652865b51d6077b0bc61954148275a6d
Skyler Layne <skylerclayne@gmail.com>
2016-05-26 09:13:19 -0400

    fix prep

--- lib/prep.rb
+++ lib/prep.rb
@@ -1,10 +1,3 @@
 module Preparations
-  class Preparations
-
-    attr_accessor :info
-
-    def initialize
-      @info = "Make sure you know the vehicle you are driving. You should know at least how to turn on/use your headlights on, high beams on, wipers on, emergency break, four ways, signal light, AC/Heat, how to adjust your mirrors, seat, seatbelt."
-    end
-  end
+  INFO = "Make sure you know the vehicle you are driving. You should know at least how to turn on/use your headlights on, high beams on, wipers on, emergency break, four ways, signal light, AC/Heat, how to adjust your mirrors, seat, seatbelt."
 end
