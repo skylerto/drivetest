8d6a77c9455b9aee388f1929e7d0866ff045ad0c
Skyler Layne <skylerclayne@gmail.com>
2016-05-26 09:03:21 -0400

    stopping

---
+++ lib/stopping/stopping.rb
@@ -0,0 +1,10 @@
+require_relative './emergency_stop'
+require_relative './stop_light'
+require_relative './stop_sign'
+
+module Stopping
+
+  INFO = "Always check your rear view mirror before slowing down/stopping. Make sure you always stop before the white line, making sure you can see a little bit of the line.
+
+    If there are vehicle in front of you, make sure you stop behind the vehicle making sure you can see their tires and a little pavement."
+end
