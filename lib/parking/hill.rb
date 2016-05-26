b5799e60f48b1ee262d593c342d0a4b8a6d7bd56
Skyler Layne <skylerclayne@gmail.com>
2016-05-26 08:37:34 -0400

    Uphill/downhill parking

--- lib/parking/hill.rb
+++ lib/parking/hill.rb
@@ -4,14 +4,24 @@ module Parking
 
     attr_accessor :how
     def initialize
-      @wheels = "left"
+      @wheels = "right"
       @how = [
         "When parking down hill indicate to the right, check mirrors and blind spots before slowing down, pull off to the side of the road.",
         "Making sure you are straight, and 30 cm away from the shoulder.",
-        "Turn the wheels all the way to the right. Put the car in park. Finally, engage the parking break."
+        "Turn the wheels all the way to the #{@wheels}. Put the car in park. Finally, engage the parking break."
 
       ]
       @index = -1
     end
+  end
+
+  class UpHill < Hill
+    def initialize
+      @wheels = "right, unless there is a curb in whcih case turn your wheels to the left"
+    end
+  end
+
+
+  class DownHill < Hill
   end
 end
